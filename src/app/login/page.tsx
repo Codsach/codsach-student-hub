aead469f5e9fdf3e77c5bbea8189af28b8a75881
Sachin .R <rsachinsachi@gmail.com>
2025-08-16 10:35:13 +0000

    login page should be open at full screen like its a new page not inside

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,8 @@
-
 import { LoginForm } from '@/components/auth/login-form';
 
 export default function LoginPage() {
   return (
-    <div className="relative flex-1 w-full flex items-center justify-center p-4 overflow-hidden">
+    <div className="relative flex-1 w-full min-h-dvh flex items-center justify-center p-4 overflow-hidden">
       <div className="absolute inset-0 z-0">
         <div className="background-animation">
           <div className="circle-1"></div>
